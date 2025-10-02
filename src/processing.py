a8853e9a45832d27a10a7a44c372a1359b4674dd
Ярослав Попов <popow.yaroslaw2012@yandex.ru>
2025-10-02 17:49:04 +0500

    Доработка docstring функций

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,9 @@
 def filter_by_state(user_list_dict: list, state: str = "EXECUTED") -> list:
     """
     Функция сортирует данные по ключу операции 'state'.
-    :param user_list_dict:
-    :param state:
-    :return:
+    :param user_list_dict: список словарей для сортировки
+    :param state: ключ 'state' для сортировки
+    :return: отсортированный список
     """
     new_list = []
     for el in user_list_dict:
@@ -15,9 +15,9 @@ def filter_by_state(user_list_dict: list, state: str = "EXECUTED") -> list:
 def sort_by_date(user_list_dict: list, descending: bool = True) -> list:
     """
     Функция сортирует данные по дате.
-    :param user_list_dict:
-    :param descending:
-    :return:
+    :param user_list_dict: список словарей для сортировки
+    :param descending: порядок сортировки (По умолчанию -  по убыванию)
+    :return: отсортированный список
     """
     new_list = sorted(user_list_dict, key=lambda date: date["date"], reverse=descending)
     return new_list
