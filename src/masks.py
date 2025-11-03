831741e816c7b855a78ffb3befafccaa069edd5b
Ярослав Попов <popow.yaroslaw2012@yandex.ru>
2025-11-03 16:33:44 +0500

    Исправление ошибок в функциях

--- src/masks.py
+++ src/masks.py
@@ -8,14 +8,14 @@ def get_mask_card_number(number_card: Optional[int] = None) -> str:
     :return:
     """
     if type(number_card) is not int:
-        return "Номер карты может быть только числовым значением!"
+        raise TypeError("Номер карты может быть только числовым значением!")
     if number_card < 0:
-        return "Номер карты не может быть отрицальным!"
+        raise ValueError("Номер карты не может быть отрицальным!")
     mask_number_card_str = str(number_card)
     if len(mask_number_card_str) == 16:
         return f"{mask_number_card_str[:4]} {mask_number_card_str[4:6]}** **** {mask_number_card_str[12:]}"
     else:
-        return "Номер карты состоит не из 16 цифр!"
+        raise ValueError("Номер карты состоит не из 16 цифр!")
 
 
 def get_mask_account(number_account: Optional[int] = None) -> str:
@@ -25,11 +25,11 @@ def get_mask_account(number_account: Optional[int] = None) -> str:
     :return:
     """
     if type(number_account) is not int:
-        return "Номер счёта может быть только числовым значением!"
+        raise TypeError("Номер счёта может быть только числовым значением!")
     if number_account < 0:
-        return "Номер счёта не может быть отрицальным!"
+        raise ValueError("Номер счёта не может быть отрицальным!")
     mask_number_account_str = str(number_account)
     if len(mask_number_account_str) >= 6:
         return f"**{mask_number_account_str[-4:]}"
     else:
-        return "Номер счёта не может быть меньше 6 цифт!"
+        raise ValueError("Номер счёта не может быть меньше 6 цифт!")
