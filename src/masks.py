ab5254b6b3a1c1344da033d802e08167a461c087
Ярослав Попов <popow.yaroslaw2012@yandex.ru>
2025-12-27 17:47:41 +0500

    Исправления номера карты на числовой для правильной работы

--- src/masks.py
+++ src/masks.py
@@ -15,23 +15,19 @@ logging.basicConfig(
 masks_logger = logging.getLogger("masks_logger")
 
 
-def get_mask_card_number(number_card: Optional[int] = None) -> str:
+def get_mask_card_number(number_card: Optional[str] = None) -> str:
     """
     Функция выдает макску карты в формате: XXXX XX** **** XXXX
     :param number_card:
     :return:
     """
     masks_logger.info("Функция запущена")
-    if type(number_card) is not int:
-        masks_logger.error("Номер карты может быть только числовым значением!")
-        raise TypeError("Номер карты может быть только числовым значением!")
-    if number_card < 0:
-        masks_logger.error("Номер карты не может быть отрицальным!")
-        raise ValueError("Номер карты не может быть отрицальным!")
-    mask_number_card_str = str(number_card)
-    if len(mask_number_card_str) == 16:
+    if type(number_card) is not str:
+        masks_logger.error("Номер карты может быть только строчным значением!")
+        raise TypeError("Номер карты может быть только строчным значением!")
+    if len(number_card) == 16:
         masks_logger.info("Функция выполнена успешно")
-        return f"{mask_number_card_str[:4]} {mask_number_card_str[4:6]}** **** {mask_number_card_str[12:]}"
+        return f"{number_card[:4]} {number_card[4:6]}** **** {number_card[12:]}"
     else:
         masks_logger.error("Номер карты состоит не из 16 цифр!")
         raise ValueError("Номер карты состоит не из 16 цифр!")
