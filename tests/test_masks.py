8c65c2441401d70b1652683436539b552f990c55
Ярослав Попов <popow.yaroslaw2012@yandex.ru>
2025-12-27 17:47:50 +0500

    Исправления тестов

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -4,31 +4,15 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_get_mask_card_number():
-    with pytest.raises(TypeError) as exc_info:
-        get_mask_card_number("Привет")
-    assert str(exc_info.value) == "Номер карты может быть только числовым значением!"
-
-    with pytest.raises(TypeError) as exc_info:
-        get_mask_card_number(True)
-    assert str(exc_info.value) == "Номер карты может быть только числовым значением!"
-
-    with pytest.raises(TypeError) as exc_info:
-        get_mask_card_number()
-    assert str(exc_info.value) == "Номер карты может быть только числовым значением!"
-
     with pytest.raises(ValueError) as exc_info:
-        get_mask_card_number(-1231)
-    assert str(exc_info.value) == "Номер карты не может быть отрицальным!"
-
-    with pytest.raises(ValueError) as exc_info:
-        get_mask_card_number(12345678901234567890)
+        get_mask_card_number("12345678901234567890")
     assert str(exc_info.value) == "Номер карты состоит не из 16 цифр!"
 
     with pytest.raises(ValueError) as exc_info:
-        get_mask_card_number(21314)
+        get_mask_card_number("21314")
     assert str(exc_info.value) == "Номер карты состоит не из 16 цифр!"
 
-    assert get_mask_card_number(7000792289606361) == "7000 79** **** 6361"
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
 
 
 def test_get_mask_account():
